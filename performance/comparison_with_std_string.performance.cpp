747f4b15c2b162addbc58b53757683259e774758
Mate Pek <mate.pek@gmail.com>
2018-05-06 21:21:43 +0700

    performance test impovements

--- performance/comparison_with_std_string.performance.cpp
+++ performance/comparison_with_std_string.performance.cpp
@@ -11,24 +11,26 @@
 #include <string>
 #include <thread>
 
-//#include "jasl_string_view.hpp"
-//#define JASL_STRING_VIEW_TYPE jasl::basic_jasl_string_view
 #include "jasl_static_string.hpp"
+#include "jasl_string.hpp"
 
 constexpr static size_t ItemCount = 100000;
-constexpr static size_t IterCount = 1;
+constexpr static size_t IterCount = 5;
 
 template <typename ItemType,
           size_t ItemCountT = ItemCount,
           size_t IterCountT = IterCount>
-void measure(const char* description,
-             const std::function<void(ItemType*)>& construct,
-             const std::function<void(ItemType&)>& destruct,
-             const std::function<void(ItemType&)>& doIt = nullptr) {
+std::pair<std::string, double> measure(
+    std::string description,
+    const std::function<void(ItemType*)>& construct,
+    const std::function<void(ItemType&)>& destruct,
+    const std::function<void(ItemType&)>& doIt = nullptr) {
   std::chrono::duration<double> durSum;
   for (size_t iter = 0; iter < IterCountT; ++iter) {
     std::unique_ptr<char[]> array(new char[ItemCountT * sizeof(ItemType)]);
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    // it has to sleep enough time to eliminate other effects as much as
+    // reasonable.
+    std::this_thread::sleep_for(std::chrono::seconds(10));
     auto start = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < ItemCountT; ++i) {
       construct(reinterpret_cast<ItemType*>(&array[i * sizeof(ItemType)]));
@@ -44,22 +46,68 @@ void measure(const char* description,
     auto stop = std::chrono::high_resolution_clock::now();
     durSum += std::chrono::duration<double>(stop - start);
   }
-  std::cout << description << (durSum / ItemCountT / IterCountT).count()
+  auto elapsed = (durSum / ItemCountT / IterCountT).count();
+  return std::pair<std::string, double>(std::move(description), elapsed);
+}
+
+void print_compare(const std::pair<std::string, double>& left,
+                   const std::pair<std::string, double>& right) {
+  std::cout << left.first << ": " << left.second << " / unit" << std::endl
+            << right.first << ": " << right.second << " / unit" << std::endl
+            << right.first << " / " << left.first << " = "
+            << static_cast<int>(100 * right.second / left.second) << " %"
             << std::endl;
 }
 
 int main() {
-  // some timing problem still there
-  measure<jasl::static_string>(
-      "small static_string: ",
+  std::cout << "This performance test compares construction and destruction of"
+            << std::endl
+            << "the jasl::static_string and the jasl::string to std::string."
+            << std::endl
+            << std::endl
+            << "ATTENTION: Are you sure you don't run any resourceful "
+               "application right now?!"
+            << std::endl
+            << "(I know its tempting to read xkcd.org while you are waiting "
+               "for the result, "
+            << std::endl
+            << "but you should preolad your articles before you measure.)"
+            << std::endl
+            << std::endl;
+#ifdef JASL_DEBUG
+  std::cout << "!!! WARNING !!! This is not an optimized build!" << std::endl
+            << std::endl;
+#endif
+
+  auto small_jasl_static_string = measure<jasl::static_string>(
+      "small jasl::static_string",
       [](jasl::static_string* ptr) { new (ptr) jasl::static_string("small"); },
       [](jasl::static_string& str) {
         using namespace jasl;
         str.~static_string();
       });
 
-  measure<jasl::static_string>(
-      "long static_string: ",
+  auto small_jasl_string_static = measure<jasl::string>(
+      "small jasl::string with static",
+      [](jasl::string* ptr) { new (ptr) jasl::string("small"); },
+      [](jasl::string& str) {
+        using namespace jasl;
+        str.~string();
+      });
+
+  auto small_std_string = measure<std::string>(
+      "small std::string",
+      [](std::string* ptr) { new (ptr) std::string("small"); },
+      [](std::string& str) {
+        using namespace std;
+        str.~string();
+      });
+
+  print_compare(small_jasl_static_string, small_std_string);
+  print_compare(small_jasl_string_static, small_std_string);
+
+  auto long_jasl_static_string = measure<jasl::static_string>(
+      "long jasl::static_string",
       [](jasl::static_string* ptr) {
         new (ptr) jasl::static_string(
             "n4iwohgoiuwhvjknvirwthbuihvnjkrwhtuinvkjrwnviuwphjewh"
@@ -71,15 +119,23 @@ int main() {
         using namespace jasl;
         str.~static_string();
       });
-  measure<std::string>("small std::string: ",
-                       [](std::string* ptr) { new (ptr) std::string("small"); },
-                       [](std::string& str) {
-                         using namespace std;
-                         str.~string();
-                       });
 
-  measure<std::string>(
-      "long std::string: ",
+  auto long_jasl_string_static = measure<jasl::string>(
+      "long jasl::string static",
+      [](jasl::string* ptr) {
+        new (ptr) jasl::string(
+            "n4iwohgoiuwhvjknvirwthbuihvnjkrwhtuinvkjrwnviuwphjewh"
+            "gvuiwnvw giuwrtgj itwrj vjern ijowthgiu whg "
+            "uipwvjprwg piuwrhtg puiwrfj prin 4irufjwe "
+            "iupnwiuptg");
+      },
+      [](jasl::string& str) {
+        using namespace jasl;
+        str.~string();
+      });
+
+  auto long_std_string = measure<std::string>(
+      "long std::string",
       [](std::string* ptr) {
         new (ptr) std::string(
             "bigger than std::string's static storage, which is "
@@ -89,5 +145,9 @@ int main() {
         using namespace std;
         str.~string();
       });
+
+  print_compare(long_jasl_static_string, long_std_string);
+  print_compare(long_jasl_string_static, long_std_string);
+
   return 0;
 }
